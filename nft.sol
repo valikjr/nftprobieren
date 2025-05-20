118b3b116a186715966d0c25bf854cd47dd0b611
valik <111908557+valikjr@users.noreply.github.com>
2025-05-20 22:41:39 +0200

    Update nft.sol

--- nft.sol
+++ nft.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT is ERC721 {
+contract MyNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -12,8 +12,13 @@ contract MyNFT is ERC721 {
 
     function mintNFT(address recipient) public returns (uint256) {
         _tokenIds.increment();
+
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
+
+        // Укажи здесь CID твоего metadata.json
+        _setTokenURI(newItemId, "ipfs://bafybeiezrubbhs6hgjtaeu25pquyqbrhry6wc4mmbtxpl32bwpnccmxidq");
+
         return newItemId;
     }
 }
